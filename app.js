13874f7d1c828963abdb850da9538171e32e6fb0
Dariusz Franczak <darius32@op.pl>
2018-05-29 23:16:00 +0100

    ex 02 completed

--- app.js
+++ app.js
@@ -1,21 +1,71 @@
-var button = document.getElementById('button');
+var button1 = document.getElementById('button1');
+var button2 = document.getElementById('button2');
 
-// basic Ajax call function
-function loadData() {
+// Ajax call function 
+function loadCustomer() {
   // Create XHR Object
   const xhr = new XMLHttpRequest();
 
   // Open
-  xhr.open('GET', 'data.txt', true);
+  xhr.open('GET', 'customer.json', true);
 
   xhr.onload = function() {
     if (this.status === 200) {
-      document.getElementById('output').innerHTML = `<h1> ${this.responseText} </h1>`
-      console.log(this.responseText);
-  }
+      const customer = JSON.parse(this.responseText);
+      const output = `
+        <ul>
+          <li>ID: ${customer.id}</li>
+          <li>Name: ${customer.name}</li>
+          <li>Company: ${customer.company}</li>
+          <li>Phone: ${customer.phone}</li>
+        </ul>
+      `;
+
+      document.getElementById('customer').innerHTML = output;
+      
+    }
   }
 
   xhr.send();
 }
 
-button.addEventListener('click', loadData);
+
+// Ajax call function 
+function loadCustomers() {
+  // Create XHR Object
+  const xhr = new XMLHttpRequest();
+
+  // Open
+  xhr.open('GET', 'customers.json', true);
+
+  xhr.onload = function() {
+    if (this.status === 200) {
+      const customers = JSON.parse(this.responseText);
+      var output;
+
+      customers.forEach(element => {
+         output += `
+        <ul>
+          <li>ID: ${element.id}</li>
+          <li>Name: ${element.name}</li>
+          <li>Company: ${element.company}</li>
+          <li>Phone: ${element.phone}</li>
+        </ul>
+      `;
+
+      
+      });
+
+      
+
+      document.getElementById('customers').innerHTML = output;
+      
+    }
+  }
+
+  xhr.send();
+}
+
+
+button1.addEventListener('click', loadCustomer);
+button2.addEventListener('click', loadCustomers);
