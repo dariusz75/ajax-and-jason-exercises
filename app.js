bc5592d5ebeadd7d3028f8ab5dba831b2fde2631
TCSL\DariuszF <dariusz.franczak@taxcomputersystems.com>
2018-05-30 12:14:41 +0100

    CRUD completed

--- app.js
+++ app.js
@@ -1,41 +1,42 @@
-var button = document.getElementById('get-jokes');
+const http = new easyHTTP;
 
-// Ajax call function 
-function getJokes(e) {
-  e.preventDefault();
-
-  const number = document.getElementById('number').value;
-  console.log(number);
-
-  // Create XHR Object
-  const xhr = new XMLHttpRequest();
-
-  // Open
-  xhr.open('GET', 'http://api.icndb.com/jokes/random/' + number, true);
-
-  xhr.onload = function() {
-    if (this.status === 200) {
-      const response = JSON.parse(this.responseText);
-      console.log(response);
-      var output = '';
-
-      response.value.forEach(element => {
-        output += `
-          <li>${element.joke}</li>
-      `;
-      });
-
-      
-
-      document.getElementById('jokes').innerHTML = output;
-      
-    }
+// Get posts from jasonplaceholder website API
+http.get('https://jsonplaceholder.typicode.com/posts', function(posts) {
+  if(posts) {
+  console.log(posts);
   }
+});
 
-  xhr.send();
-}
+// Get single post from jasonplaceholder website API
+http.get('https://jsonplaceholder.typicode.com/posts/1', function(post) {
+  if(post) {
+  console.log(post);
+  }
+});
 
+// Create Data
+const data = {
+  title: 'Test post',
+  body: 'This is a new test post'
+};
 
+// Create Post
+http.post('https://jsonplaceholder.typicode.com/posts', data, function(post) {
+  if(post) {
+    console.log(post);
+    }
+});
 
+// Update Post
+http.put('https://jsonplaceholder.typicode.com/posts/1', data, function(post) {
+  if(post) {
+    console.log(post);
+    }
+});
 
-button.addEventListener('click', getJokes);
+// Delete post
+http.delete('https://jsonplaceholder.typicode.com/posts/1', function(response) {
+  if(response) {
+  console.log(response);
+  }
+});
